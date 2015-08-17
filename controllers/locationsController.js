c5cd4ea15503a2310561877ddb6031e24951f5f5
Prity Patel <pritypatel714@gmail.com>
2015-08-17 16:38:22 -0700

    updating routes, on route index file, and controllers, updated json with passport

---
+++ controllers/locationsController.js
@@ -0,0 +1,2 @@
+var Location = require('../models/Location');
+
--- models/User.js
+++ models/User.js
@@ -1,18 +1,17 @@
 // Loading the Mongoose library
 var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+
 
 // Loading Location in order to reference locations in the User Schema
 var Location = require('./Location');
 
 // Creating Schema to define structure of 'users' in our DB
-var UserSchema = new mongoose.Schema({
-  name:     String,
-  email:    String,
-  password: String,
-  locations: [{type: mongoose.Schema.Types.ObjectId, ref: 'Location'}]
+var User = new mongoose.Schema({
+  name: String,
+  location: [{type: mongoose.Schema.Types.ObjectId, ref: 'Location'}]
 });
 
-// Creating User model that maps to UserSchema
-var User = mongoose.model('User', UserSchema);
+User.plugin(passportLocalMongoose);
 
-module.exports = User;
+module.exports = mongoose.model('User', User);
