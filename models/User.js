7268f6c90b6caab67268c70493f55915f5cc3565
Tiffany Chang <chang.tiffany@gmail.com>
2015-08-17 13:48:20 -0700

    Cleaned up missing reference to routes/users; added references to Location schema

--- models/User.js
+++ models/User.js
@@ -1,15 +1,16 @@
 // Loading the Mongoose library
 var mongoose = require('mongoose');
+var Location = require('./Location');
 
 // Calling Location in order to embed locations in the User Schema
-var Location = mongoose.model('Location');
+// var Location = mongoose.model('Location');
 
 // Creating Schema to define structure of 'users' in our DB
-var User = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
   name:     String,
   email:    String,
   password: String,
-  locations: [Location.schema]
+  locations: [{type: mongoose.Schema.Types.ObjectId, ref: 'Location'}]
 });
 
 // Creating User model that maps to UserSchema
