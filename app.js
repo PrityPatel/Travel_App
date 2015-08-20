ccb574fb5e35ee9263422c60feeeb021e6d33c40
Prity Patel <pritypatel714@gmail.com>
2015-08-20 16:11:54 -0700

    requiring dotenv

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// require('dotenv').load();
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -32,7 +32,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 
