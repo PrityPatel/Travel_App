6c89c67b39c67f1797732af217943fb52083d896
Prity Patel <pritypatel714@gmail.com>
2015-08-20 08:51:40 -0700

    changing locations show for New York to Manhattan until regex code is implemented

--- api/numbeo.js
+++ api/numbeo.js
@@ -2,7 +2,6 @@ var request = require('request');
 var baseURL = "http://www.numbeo.com/api/city_prices?";
 var clientID = "api_key=n584lhb87k93ms&query=";
 
-//http://www.numbeo.com/api/city_prices?api_key=n584lhb87k93ms&query=London
 
 var get = function(locationName, callback) {
   request(baseURL + clientID + locationName, function(error, response, body) {
