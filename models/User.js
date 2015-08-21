02653af6625b4d8bb8ad6f084e636a2442116e23
Tiffany Chang <chang.tiffany@gmail.com>
2015-08-20 20:58:07 -0700

    Cleaning up User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-
-// // Loading Location in order to reference locations in the User Schema
-// var Location = require('./Location');
-
 // Creating Schema to define structure of 'users' in our DB
 var UserSchema = new mongoose.Schema({
   name: String,
