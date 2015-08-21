db255a8076c94e8ff52b8eaa19364d442eabc2d1
Tiffany Chang <chang.tiffany@gmail.com>
2015-08-20 18:33:34 -0700

    merged

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -39,49 +39,51 @@ var create = function(req, res){
 var show = function(req, res, next) {
   //get instagram data
   var instagramData;
+
   instagram.get(req.params.id, function(stringdata){
     instagramData = JSON.parse(stringdata);
+
     instagramData = instagramData.data.map(function(post){
       return post.images.standard_resolution.url;
     });
 
     console.log('Array of Instagram Photo URLs: ' + instagramData);
 
-//get numbeo data of average prices for certain items per city
-  var numbeoData, locationName;
+    //get numbeo data of average prices for certain items per city
+    var numbeoData, locationName;
 
-  if (req.params.id == "728021537") locationName = "London";
-  if (req.params.id == "523722982") locationName = "Mumbai";
-  if (req.params.id == "35932492") locationName = "Manhattan";
-  if (req.params.id == "301343440") locationName = "Paris";
-  if (req.params.id == "167346217") locationName = "San+Francisco";
-  if (req.params.id == "213193384") locationName = "Shanghai";
+    if (req.params.id == "728021537") locationName = "London";
+    if (req.params.id == "523722982") locationName = "Mumbai";
+    if (req.params.id == "35932492")  locationName = "Manhattan";
+    if (req.params.id == "301343440") locationName = "Paris";
+    if (req.params.id == "167346217") locationName = "San+Francisco";
+    if (req.params.id == "213193384") locationName = "Shanghai";
 
-  numbeo.get(locationName, function(stringdata){
-    // first turn JSON into an object, then
-    // get the `prices` array inside it and assign it to var `numbeoData`
-    numbeoData = JSON.parse(stringdata).prices;
-    // turn numbeoData into an array of objects with 'name' and 'price' as key names
-    numbeoData = numbeoData.map(function(item) {
-      return {name: item.item_name, price: item.average_price};
-    });
+    numbeo.get(locationName, function(stringdata){
+      // first turn JSON into an object, then
+      // get the `prices` array inside it and assign it to var `numbeoData`
+      numbeoData = JSON.parse(stringdata).prices;
+      // turn numbeoData into an array of objects with 'name' and 'price' as key names
+      numbeoData = numbeoData.map(function(item) {
+        return {name: item.item_name, price: item.average_price};
+      });
 
-    numbeoData.forEach(function(pricegroup) {
-      console.log('Item: ' + pricegroup.name + ' price:' + pricegroup.price);
-    });
+      numbeoData.forEach(function(pricegroup) {
+        console.log('Item: ' + pricegroup.name + ' price:' + pricegroup.price);
+      });
 
-    Location.findById(req.params.id, function(location) {
-      res.render('locations/show',
-        {
-          location: locationName,
-          user:    req.user,
-          instagramData: instagramData,
-          numbeoData: numbeoData
+      Location.findById(req.params.id, function(location) {
+        res.render('locations/show',
+          {
+            location: locationName,
+            user:    req.user,
+            instagramData: instagramData,
+            numbeoData: numbeoData
         });
       });
+
     });
   });
-
 };
 
 module.exports = {
